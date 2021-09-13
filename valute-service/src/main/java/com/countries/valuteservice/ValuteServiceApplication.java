cff57e04444b6fccc204c12c02f10805fc29e254
Slavko <firepoint.pro@gmail.com>
2021-09-13 18:36:28 +0300

    Initial commit

---
+++ valute-service/src/main/java/com/countries/valuteservice/ValuteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.countries.valuteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValuteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValuteServiceApplication.class, args);
+	}
+
+}
---
+++ valute-service/src/test/java/com/countries/valuteservice/ValuteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.countries.valuteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValuteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
