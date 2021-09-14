9bb33c98e62e8d32f0d5fb329fd76a04ce537dfe
Slavko <firepoint.pro@gmail.com>
2021-09-14 13:59:04 +0300

    Добавлен единый сервер конфигурации и к нему подключен countries service

---
+++ countries-service/src/main/java/com/countries/countriesservice/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.countries.countriesservice.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RefreshScope
+public class TestController {
+
+    @Value("${test}")
+    private String name;
+
+    @GetMapping("/test")
+    public String printFromConfigServer() {
+        return name;
+    }
+
+}
